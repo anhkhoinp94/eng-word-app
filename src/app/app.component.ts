13f1bace102e1845b78e85645a80e0b46a8b8a45
anhkhoinp94 <73807342+anhkhoinp94@users.noreply.github.com>
2022-08-07 17:40:38 +0700

    2022-08-07_0540 PM

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ import wordList7 from '../assets/json/words7.json';
 import wordList8 from '../assets/json/words8.json';
 import wordList9 from '../assets/json/words9.json';
 import wordList10 from '../assets/json/words10.json';
-// import wordList11 from '../assets/json/words11.json';
+import wordList11 from '../assets/json/words11.json';
 // import wordList12 from '../assets/json/words12.json';
 // import wordList13 from '../assets/json/words13.json';
 // import wordList14 from '../assets/json/words14.json';
@@ -70,7 +70,7 @@ export class AppComponent {
   no = 1;
   min = 1;
   // max = 565;
-  max = 120;
+  max = 132;
   vnWord1 = '';
   vnWord2 = '';
   enWord1 = '';
@@ -81,7 +81,7 @@ export class AppComponent {
   countSawWords = 0;
 
   constructor() {
-    this.words = this.words.concat(wordList1, wordList2, wordList3, wordList4, wordList5, wordList6, wordList7, wordList8, wordList9, wordList10);
+    this.words = this.words.concat(wordList1, wordList2, wordList3, wordList4, wordList5, wordList6, wordList7, wordList8, wordList9, wordList10, wordList11);
     let id = this.getRandomArbitrary(this.min, this.max);
     let word = this.words.find((obj) => {
       return obj.id === id;
