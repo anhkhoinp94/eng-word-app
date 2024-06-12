b3c32db5fee88e79e138220482339b24d08affa4
npakhoi <anhkhoi.np94@gmail.com>
2024-06-12 07:58:23 +0700

    2024-06-12_0758 AM

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -74,9 +74,9 @@ export class AppComponent {
   see = false;
   no = 1;
   id = 1;
-  min = 1;
+  min = 200;
   // max = 603;
-  max = 236;
+  max = 252;
   vnWord1 = '';
   vnWord2 = '';
   enWord1 = '';
